dd7212d4c900663f8a4d523d96a15875ac7a04a5
Francisco-Silvaecom <eudeseudinho28@gmail.com>
2025-11-23 22:17:24 -0300

    Create Navbar.js

---
+++ pages/components/Navbar.js
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import styles from '../styles/Home.module.css'; // Onde estão os estilos da nav
+
+const Navbar = () => {
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.navContainer}>
+                {/* Nome do Site (Link para a Home) */}
+                <Link href="/" passHref legacyBehavior>
+                    <a className={styles.navLogo}>MidiaDown</a>
+                </Link>
+
+                {/* Links de Navegação */}
+                <div className={styles.navLinks}>
+                    <Link href="/" passHref legacyBehavior>
+                        <a className={styles.navItem}>Início</a>
+                    </Link>
+                    <Link href="/about" passHref legacyBehavior>
+                        <a className={styles.navItem}>Sobre Nós</a>
+                    </Link>
+                    <Link href="/privacy" passHref legacyBehavior>
+                        <a className={styles.navItem}>Privacidade</a>
+                    </Link>
+                    <Link href="/terms" passHref legacyBehavior>
+                        <a className={styles.navItem}>Termos</a>
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
